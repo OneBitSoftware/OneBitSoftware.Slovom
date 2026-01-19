88278a00c72d7ee4ba1535bfd90c6a815d1ac0dc
Yulian Ashikov <y.ashikov@onebitsoftware.net>
2026-01-19 11:47:10 +0200

    Added tests for negative values

--- src/Tests/ConvertTests.cs
+++ src/Tests/ConvertTests.cs
@@ -16,6 +16,7 @@ public class ConvertTests
     [InlineData(11, "единадесет лева")]
     [InlineData(12, "дванадесет лева")]
     [InlineData(19, "деветнадесет лева")]
+    [InlineData(100, "сто лева")]
     [InlineData(0.0, "нула лева")]
     [InlineData(0.1, "десет стотинки")]
     [InlineData(1.20, "един лев и 20 ст.")]
@@ -68,9 +69,28 @@ public class ConvertTests
     }
     
     [Theory]
-    [InlineData(-1, "един лев")] // Assuming negative values are treated as positive
-    [InlineData(-0.01, "една стотинка")] // Assuming negative values are treated as positive
-    [InlineData(-32478.27, "тридесет и две хиляди четиристотин седемдесет и осем лева и 27 ст.")]
+    [InlineData(-0.01, "Минус една стотинка")]
+    [InlineData(-0.02, "Минус две стотинки")]
+    [InlineData(-0.10, "Минус десет стотинки")]
+    [InlineData(-0.19, "Минус деветнадесет стотинки")]
+    [InlineData(-0.20, "Минус 20 ст.")]
+    [InlineData(-1, "Минус един лев")]
+    [InlineData(-1.01, "Минус един лев и 1 ст.")]
+    [InlineData(-1.10, "Минус един лев и 10 ст.")]
+    [InlineData(-2, "Минус два лева")]
+    [InlineData(-11, "Минус единадесет лева")]
+    [InlineData(-20, "Минус двадесет лева")]
+    [InlineData(-21, "Минус двадесет и един лева")]
+    [InlineData(-100, "Минус сто лева")]
+    [InlineData(-101, "Минус сто и един лева")]
+    [InlineData(-111, "Минус сто и единадесет лева")]
+    [InlineData(-1000, "Минус хиляда лева")]
+    [InlineData(-1001, "Минус хиляда и един лева")]
+    [InlineData(-2000, "Минус две хиляди лева")]
+    [InlineData(-10000, "Минус десет хиляди лева")]
+    [InlineData(-21000, "Минус двадесет и една хиляди и нула лева")]
+    [InlineData(-32478.27, "Минус тридесет и две хиляди четиристотин седемдесет и осем лева и 27 ст.")]
+    [InlineData(-0.00, "нула лева")]
     public void NumberToWordsBG_ShouldReturnCorrectWordsForNegativeValues(decimal number, string expected)
     {
         // Act
@@ -94,6 +114,7 @@ public class ConvertTests
     [InlineData(1.20, "едно евро и 20 ц.")]
     [InlineData(1.3, "едно евро и 30 ц.")]
     [InlineData(1.02, "едно евро и 2 ц.")]
+    [InlineData(100, "сто евро")]
     [InlineData(118, "сто и осемнадесет евро")]
     [InlineData(123, "сто двадесет и три евро")]
     [InlineData(1000, "хиляда евро")]
@@ -133,6 +154,30 @@ public class ConvertTests
     [InlineData(32008.27, "тридесет и две хиляди и осем евро и 27 ц.")]
     [InlineData(99999.99, "деветдесет и девет хиляди деветстотин деветдесет и девет евро и 99 ц.")]
     public void NumberToWordsEuro_ShouldReturnCorrectWords(decimal number, string expected)
+    {
+        // Act
+        var result = NumbersToWords.Convert(number, CurrencyDescriptor.Euro);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+    
+    [Theory]
+    [InlineData(-0.01, "Минус един евроцент")]
+    [InlineData(-0.02, "Минус две евроцента")]
+    [InlineData(-0.10, "Минус десет евроцента")]
+    [InlineData(-0.19, "Минус деветнадесет евроцента")]
+    [InlineData(-0.20, "Минус 20 ц.")]
+    [InlineData(-1, "Минус едно евро")]
+    [InlineData(-1.01, "Минус едно евро и 1 ц.")]
+    [InlineData(-2, "Минус две евро")]
+    [InlineData(-10.20, "Минус десет евро и 20 ц.")]
+    [InlineData(-11, "Минус единадесет евро")]
+    [InlineData(-21, "Минус двадесет и едно евро")]
+    [InlineData(-100, "Минус сто евро")]
+    [InlineData(-1000, "Минус хиляда евро")]
+    [InlineData(-32478.27, "Минус тридесет и две хиляди четиристотин седемдесет и осем евро и 27 ц.")]
+    public void NumberToWordsEuro_ShouldReturnCorrectWordsForNegativeValues(decimal number, string expected)
     {
         // Act
         var result = NumbersToWords.Convert(number, CurrencyDescriptor.Euro);
